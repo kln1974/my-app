01680f16c6b7ab8a188282decfb862f080805d80
kln1974 <kln1974@gmail.com>
2019-09-23 12:08:54 +0530

    init

---
+++ src/test/java/in/gov/isro/TestCal.java
@@ -0,0 +1,21 @@
+package in.gov.isro;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCal {
+
+	@Test
+	public void testAdd() {
+		Calculator calc = new Calculator();
+		int result = calc.add(5, 5);
+		assertEquals(10, result);
+	}
+	@Test
+	public void testMultiply() {
+		Calculator calc = new Calculator();
+		int result = calc.multiply(5, 5);
+		assertEquals(25, result);
+	}
+}
